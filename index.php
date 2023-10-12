2878d129c3bb1ef86bbb85d64a378054e603f034
mateusz.pluzek <mateusz.pluz@gmail.com>
2023-10-12 15:03:11 +0200

    Added basic html structure

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
+<HTML>
+    <HEAD>
+        <title>Wdpai</title>
+        <meta charset = "UTF-8">
+        <meta name = "keywords" content = "PHP, HTML, Docker">
+        <meta name = "description" content = "Wdpai project">
+        <meta name = "author" content = "Mateusz Płużek">
+        <style>
+            .DivTest {
+                padding: 5%;
+                text-align: center;
+                font-size: 30px;
+            }
+        </style>
 
-echo "hello world";
+    </HEAD>
+
+    <BODY>
+        <div id = "content" class = "DivTest">
+            <b>
+            <?php
+                echo "Hello world"
+            ?>
+            </b>
+        </div>
+    </BODY>
+
+</HTML>
+
