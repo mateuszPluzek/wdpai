5946341ee6e56bf417bbade84778de0f1a186825
mateusz.pluzek <mateusz.pluz@gmail.com>
2024-01-24 01:27:24 +0100

    working database

--- src/models/User.php
+++ src/models/User.php
@@ -6,31 +6,42 @@ class User
 {
     private $email;
     private $password;
-    private $name;
-    private $surname;
+    private $user_type;
+    private $salt;
+    private $id_user;
+    private $id_user_detail;
 
-    public function __construct(string $email, string $password, string $name, string $surname) {
+    public function __construct(string $email, string $password, string $user_type, string $salt, int $id_user, int $id_user_detail) {
         $this->email = $email;
         $this->password = $password;
-        $this->name = $name;
-        $this->surname = $surname;
-
+        $this->user_type = $user_type;
+        $this->salt = $salt;
+        $this->id_user = $id_user;
+        $this->id_user_detail = $id_user_detail;
     }
 
     public function getEmail(): string
     {
         return $this->email;
     }
-    public function getName(): string
-    {
-        return $this->name;
-    }
     public function getPassword(): string
     {
         return $this->password;
     }
-    public function getSurname(): string
+    public function getIdUser(): int
     {
-        return $this->surname;
+        return $this->id_user;
+    }
+    public function getIdUserDetail(): int
+    {
+        return $this->id_user_detail;
+    }
+    public function getSalt(): string
+    {
+        return $this->salt;
+    }
+    public function getUserType(): string
+    {
+        return $this->user_type;
     }
 }
